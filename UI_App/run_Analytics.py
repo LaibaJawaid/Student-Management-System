300ee20629c78b1d7b0e31c93db23a30488fe6ed
Laiba Jawaid <laibajawaid77@gmail.com>
2025-12-23 14:06:19 +0500

    Delete UI_App directory

--- UI_App/run_Analytics.py
+++
@@ -1,7 +0,0 @@
-from Analytics.dashApp.home import app
-
-if __name__ == "__main__":
-    app.run(
-        debug=True,
-        port=8050
-    )
--- UI_App/script.js
+++
@@ -1,2 +0,0 @@
-// Future JS interactions can be added here
-console.log("University Management System Loaded Successfully 🚀");
